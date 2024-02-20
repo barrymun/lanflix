982c47f06569e0273b15cc0c34889d5d1b2525d3
Neil Murphy <barrymun@tcd.ie>
2024-02-20 12:18:07 +0000

    working docker server with MOVIES_DIRECTORY specified on startup (wip)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 
+import { createReadStream } from "fs";
 import { createServer } from "http";
+import { join } from "path";
 
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -20,6 +22,34 @@ import express from "express";
   const jsonParser = bodyParser.json();
 
   app.use(cors(corsOptions));
+
+  app.get("/stream/:filename", (req, res) => {
+    try {
+      const { filename } = req.params;
+      console.log({ filename });
+      // const moviePath = join(__dirname, "/Users/barrymun/Documents/movies", filename);
+      const moviePath = join("/usr/src/movies", "Alien DC (1979) [1080p]/Alien.Directors.Cut.1979.1080p.mp4");
+      console.log({ moviePath });
+
+      // Check if the file exists
+      // const fileExists = existsSync(moviePath);
+      // if (!fileExists) {
+      //   return res.status(404).json({ error: "File not found" });
+      // }
+
+      // // Set appropriate headers for streaming
+      // res.setHeader("Content-Type", "video/mp4");
+
+      // // Stream the movie to the client
+      const stream = createReadStream(moviePath);
+      console.log({ stream });
+      // stream.pipe(res);
+      res.status(200).send("OK");
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
 
   server.listen(3001);
 })();
